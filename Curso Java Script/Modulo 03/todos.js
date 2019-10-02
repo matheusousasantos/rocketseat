79187445c534166a0ac06f53b074258037b76e58
Matheus Sousa <matheus.ads.study@gmail.com>
2019-10-02 14:57:33 -0300

    # Aula 03 - Renderizando Todos

--- Curso Java Script/Modulo 03/todos.js
+++ Curso Java Script/Modulo 03/todos.js
@@ -7,3 +7,17 @@ var todos = [
 	'Estudar',
 	'Acessar comunidade da Rocketseat'
 ];
+
+function renderTodos() {
+
+	for( todo of todos ) {
+		var todoElement = document.createElement('li');
+		var todoText = document.createTextNode(todo);
+
+		todoElement.appendChild(todoText);
+		listElement.appendChild(todoElement);
+	}
+
+}
+
+renderTodos();
