28abf02eb197463b979f6990dc0b8c02c94435c2
Matheus Sousa <matheus.ads.study@gmail.com>
2019-10-03 07:51:10 -0300

    # Aula 05 - Excluindo Todo

--- Curso Java Script/Modulo 03/todos.js
+++ Curso Java Script/Modulo 03/todos.js
@@ -16,7 +16,21 @@ function renderTodos() {
 		var todoElement = document.createElement('li');
 		var todoText = document.createTextNode(todo);
 
-		todoElement.appendChild(todoText);
+		var linkElement = document.createElement('a');
+
+		linkElement.setAttribute('href', '#');
+
+		var pos = todos.indexOf(todo);
+		linkElement.setAttribute('onclick', 
+			'deleteTodo( ' + pos + ' )');
+
+		var linkText = document.createTextNode('Excluir');
+
+				linkElement.appendChild(linkText);
+
+			todoElement.appendChild(todoText);
+			todoElement.appendChild(linkElement);
+
 		listElement.appendChild(todoElement);
 	}
 
@@ -34,3 +48,10 @@ function addTodo() {
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+
+	todos.splice(pos, 1);
+	renderTodos();
+
+}
