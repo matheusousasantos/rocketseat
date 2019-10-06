489ce20a36527fe408ac1357f2b97a4b94ce0504
Matheus Sousa <matheus.ads.study@gmail.com>
2019-10-06 20:55:13 -0300

    # Aula 02 -  Promises

--- Curso Java Script/Modulo 04/main.js
+++ Curso Java Script/Modulo 04/main.js
@@ -1,11 +1,26 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET','https://api.github.com/users/matheusousasantos');
-xhr.send(null);
+var minhaPromise = function() {
+	return new Promise( function(resolve, reject){
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET','https://api.github.com/users/matheusousasantos');
+		xhr.send(null);
 
-xhr.onreadystatechange = function() {
+		xhr.onreadystatechange = function() {
+			if( xhr.readyState === 4 ) {
 
-	if( xhr.readyState === 4 )  {
-		console.log(JSON.parse(xhr.responseText));
-	}
-
+				if(xhr.status === 200){
+					resolve(JSON.parse(xhr.responseText));
+				} else {
+					reject('Erro na requisição');
+				}
+			}
+		}
+	} ); 
 }
+
+minhaPromise()
+.then(function( response ) {
+	console.log( response );
+})
+.catch(function( error ) {
+	console.warn( error );
+});
