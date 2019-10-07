5a4efc1b3e81d769b1f5cbe299fc661c0d3c6e1a
Matheus Sousa <matheus.ads.study@gmail.com>
2019-10-06 23:56:35 -0300

    # Aula 04 - Classes

--- Curso-Java-Script-ES6/main.js
+++ Curso-Java-Script-ES6/main.js
@@ -1,11 +1,50 @@
-alert('Text');
-
-class Teste {
-	metodo() {
-
+class List {
+	constructor() {
+		this.data = [];
 	}
 
-	outro() {
-		
+
+	addData(data) {
+		this.data.push(data);
+		console.log(this.data);
 	}
 }
+
+
+class TodoList extends List {
+
+	constructor() {
+		super();
+		this.todos = [];
+		this.usuario = 'Matheus';
+	}
+
+	mostrarUsuario() {
+		console.log(this.usuario);
+	}
+
+	addTodo() {
+		this.todos.push('Novo Tudo');
+		console.log(this.todos);
+	}
+
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotudo').onclick = function() {
+	MinhaLista.addTodo();
+}
+
+MinhaLista.mostrarUsuario();
+MinhaLista.addData('111');
+
+class Matematica {
+
+	static soma(a,b) {
+		return a + b;
+	}
+
+}
+
+console.log(Matematica.soma(10, 10));
