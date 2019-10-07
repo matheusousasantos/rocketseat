ee63a22bd9f7c6ce9cdbf6f48ba98560bab4c3da
Matheus Sousa <matheus.ads.study@gmail.com>
2019-10-06 21:13:44 -0300

    # Aula 03 - Utilizando Axios

--- Curso Java Script/Modulo 04/main.js
+++ Curso Java Script/Modulo 04/main.js
@@ -1,25 +1,6 @@
-var minhaPromise = function() {
-	return new Promise( function(resolve, reject){
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET','https://api.github.com/users/matheusousasantos');
-		xhr.send(null);
-
-		xhr.onreadystatechange = function() {
-			if( xhr.readyState === 4 ) {
-
-				if(xhr.status === 200){
-					resolve(JSON.parse(xhr.responseText));
-				} else {
-					reject('Erro na requisição');
-				}
-			}
-		}
-	} ); 
-}
-
-minhaPromise()
+axios.get('https://api.github.com/users/matheusousasantos')
 .then(function( response ) {
-	console.log( response );
+	console.log( response.data.avatar_url );
 })
 .catch(function( error ) {
 	console.warn( error );
