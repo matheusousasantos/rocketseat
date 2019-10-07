281aef149aa2989f88a4c5925b6df2ffc0303444
Matheus Sousa <matheus.ads.study@gmail.com>
2019-10-07 08:43:07 -0300

    # Aula 05 - Const & Let

--- Curso-Java-Script-ES6/main.js
+++ Curso-Java-Script-ES6/main.js
@@ -1,50 +1,10 @@
-class List {
-	constructor() {
-		this.data = [];
-	}
+function teste(x) {
+ let y = 2;
 
-
-	addData(data) {
-		this.data.push(data);
-		console.log(this.data);
-	}
-}
-
-
-class TodoList extends List {
-
-	constructor() {
-		super();
-		this.todos = [];
-		this.usuario = 'Matheus';
-	}
-
-	mostrarUsuario() {
-		console.log(this.usuario);
-	}
-
-	addTodo() {
-		this.todos.push('Novo Tudo');
-		console.log(this.todos);
-	}
+ if(x > 5) {
+ 	console.log(x, y);
+ }
 
 }
 
-var MinhaLista = new TodoList();
-
-document.getElementById('novotudo').onclick = function() {
-	MinhaLista.addTodo();
-}
-
-MinhaLista.mostrarUsuario();
-MinhaLista.addData('111');
-
-class Matematica {
-
-	static soma(a,b) {
-		return a + b;
-	}
-
-}
-
-console.log(Matematica.soma(10, 10));
+teste(10);
