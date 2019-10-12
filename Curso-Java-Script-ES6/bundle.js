ecb1aa88fb222a28c63c3ecac47a0170e97de019
Matheus Sousa <matheus.ads.study@gmail.com>
2019-10-12 18:16:34 -0300

    #Aula 11 - Template Literals

--- Curso-Java-Script-ES6/bundle.js
+++ Curso-Java-Script-ES6/bundle.js
@@ -1,55 +1,6 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+var nome = "Matheus";
+var idade = 26; //console.log('Meu nome é '+ nome +' e tenho '+ idade + ' anos')
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-/* REST */
-
-/* Objeto */
-//const usuario = {
-//	nome: 'Matheus',
-//	idade: 26,
-//	empresa: 'Mandy'
-//};
-//const { nome , ...resto } = usuario;
-//console.log(nome);
-//console.log(resto);
-
-/* Função */
-//const arr = [1,2,3,4];
-//const [ a, b, ...c ] = arr;
-//console.log(a);
-//console.log(b);
-//console.log(c);
-
-/*Parâmetro de funções*/
-function soma() {
-  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
-    params[_key] = arguments[_key];
-  }
-
-  return params.reduce(function (total, next) {
-    return total + next;
-  });
-}
-
-console.log(soma(10, 10, 30)); //SPREAD
-
-var arr1 = [1, 2, 3];
-var arr2 = [4, 5, 6];
-var arr3 = [].concat(arr1, arr2);
-console.log(arr3);
-var usuario1 = {
-  nome: 'Matheus',
-  idade: 26,
-  empresa: 'Rockseat'
-};
-
-var usuario2 = _objectSpread({}, usuario1, {
-  nome: 'Luma'
-});
-
-console.log(usuario2);
+console.log("Meu nome \xE9 ".concat(nome, " e \n\ttenho ").concat(idade, " anos"));
--- Curso-Java-Script-ES6/main.js
+++ Curso-Java-Script-ES6/main.js
@@ -1,50 +1,7 @@
+const nome = "Matheus";
+const idade = 26;
 
-/* REST */
+//console.log('Meu nome é '+ nome +' e tenho '+ idade + ' anos')
 
-/* Objeto */
-
-//const usuario = {
-//	nome: 'Matheus',
-//	idade: 26,
-//	empresa: 'Mandy'
-//};
-
-//const { nome , ...resto } = usuario;
-
-//console.log(nome);
-//console.log(resto);
-
-/* Função */
-
-//const arr = [1,2,3,4];
-//const [ a, b, ...c ] = arr;
-
-//console.log(a);
-//console.log(b);
-//console.log(c);
-
-/*Parâmetro de funções*/
-
-function soma(...params) {
-	return params.reduce((total, next) => total + next);
-}
-
-console.log(soma(10, 10, 30));
-
-
-//SPREAD
-
-const arr1 = [1,2,3];
-const arr2 = [4,5,6];
-const arr3 = [ ...arr1, ...arr2 ];
-
-console.log(arr3);
-
-const usuario1 = {
-	nome: 'Matheus',
-	idade: 26,
-	empresa: 'Rockseat'
-}
-
-const usuario2 = { ...usuario1, nome: 'Luma' };
-console.log(usuario2);
+console.log(`Meu nome é ${nome} e 
+	tenho ${idade} anos` )
