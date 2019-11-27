a09bbb41b7d09c6cd7f8031d0fdcf0e9b021b3ad
matheus sousa <matheus.ads.study@gmail.com>
2019-11-27 14:55:20 -0300

    ( Nodejs ) #Aula 05 - Utilizando Nodemon

--- Node-js/node-api/server.js
+++ Node-js/node-api/server.js
@@ -2,17 +2,8 @@ const express = require('express')
 
 const app = express(); 
 
-//Toda vez que o usuário acessar a rota...
 app.get('/', (rep, res) => {
-    res.send('Hello Word!');
+    res.send('Olá, Mundo!');
 });
-
-//Função que recebe dois parâmetros
-
-/*rep - Requisição que estamos fazendo ao servidor contém todos os 'detalhes 
-        possíveis dessa requisição'.
-
-  res - Resposta que iremos dar para a requisição.
-*/
 
 app.listen(3001);
