0286575884bc7e8797d40652c4fcd02f11df556c
matheus sousa <matheus.ads.study@gmail.com>
2019-11-27 20:22:56 -0300

    ( NodeJs ) # Aula 07 - Conectando Database

--- Node-js/node-api/server.js
+++ Node-js/node-api/server.js
@@ -1,7 +1,14 @@
-const express = require('express') 
+const express = require('express');
+const mongoose = require('mongoose');
 
+//Inciando o app
 const app = express(); 
 
+//Iniciando o Banco de dados
+mongoose.connect('mongodb+srv://matheusousasantos:ribossomos2019@cluster0-hpnpm.mongodb.net/omnistack8?retryWrites=true&w=majority',
+{useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
+//Primeira rota
 app.get('/', (rep, res) => {
     res.send('Olá, Mundo!');
 });
