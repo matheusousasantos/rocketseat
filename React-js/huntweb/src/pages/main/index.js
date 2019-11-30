e95e6565bdfaa3bc495adf822702a65565a90058
matheus sousa <matheus.ads.study@gmail.com>
2019-11-30 15:28:43 -0300

    ( ReactJs ) # Aula 06 - Listando produtos

--- React-js/huntweb/src/pages/main/index.js
+++ React-js/huntweb/src/pages/main/index.js
@@ -3,19 +3,42 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+//  O estado é sempre um objeto
+    state = {
+//      Aqui podemos armazenar as variáveis que
+//      quizermos
 
-//  Método:
-//  Assim que o componente for mostrado em tela
+        products: [] 
+
+    }
+
     componentDidMount() {
         this.loadProducts();
     }
 
     loadProducts = async () => {
         const response = await api.get(`/products`);
-        console.log(response.data.docs);
+        
+//      Armazeando os produtos
+//      Será as informações que eu quero atualizar
+        this.setState({ 
+            products: response.data.docs 
+        });
+    
     }
 
     render() {
-        return <h1>Hello Rocketeat</h1>
+
+        return (
+
+//          Mostrando o produto em tela
+            <div className="product-list">
+
+                { this.state.products.map(product => (
+                    <h2 key={product._id}>{ product.title }</h2>
+                ))}
+            </div>
+        );
+
     }
 }
