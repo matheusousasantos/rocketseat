22e7ec1c55fe0836f751ea0f38035311a8aec9f6
matheus sousa <matheus.ads.study@gmail.com>
2019-12-03 09:47:46 -0300

    ( ReactJs ) # Aula 09 - Navegando pro detalhe

--- React-js/huntweb/src/pages/main/index.js
+++ React-js/huntweb/src/pages/main/index.js
@@ -12,8 +12,6 @@ export default class Main extends Component {
     state = {
         products: [],
         productInfo: {},
-
-//      Página que inicia a nossa aplicação
         page: 1,
     }
 
@@ -35,36 +33,23 @@ export default class Main extends Component {
     prevPage = () => {
         const { page, productInfo } = this.state;
 
-//      Não tenho nenhuma página abaixo de 1 então eu retorno
         if(page === 1) return;
 
-        const pageNumber = page - 1; //Pego a página anterior
-
-//      Vou passar a página a ser acessada na URL
+        const pageNumber = page - 1;
         this.loadProducts( pageNumber );
 
     }
 
     nextPage = () => {
 
-//  Busca qual a página atual que estou e o productInfo do estado
     const { page, productInfo } = this.state;
-
-//  Se a página que estamos atualmente já é a ultima página
-//  productInfo.pages - retorna o número total de páginas
-//  Se já tivermos na ultima página não faça nada só retorna
     if( page === productInfo.pages ) return;
-
-//  Se não...
-    const pageNumber = page + 1; //Vou pegar a próxima página
-    
-//  Vou passar a página a ser acessada na URL
+    const pageNumber = page + 1;
     this.loadProducts( pageNumber );
 
     }
 
     render() {
-//      Vai buscar a variável products dentro de state
         const { products, page, productInfo } = this.state;
 
         return (
--- React-js/huntweb/src/pages/product/index.js
+++ React-js/huntweb/src/pages/product/index.js
@@ -1,7 +1,42 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
+
+import './style.css';
 
 export default class Product extends Component {
+    
+    state = {
+        product: {}
+    };
+
+    async componentDidMount() {
+
+//      Acessa os parâmetros que estão na rota:
+//      this.props.match.params        
+
+
+        const { id } = this.props.match.params;
+
+        const response = await api.get(`/products/${ id }`);
+        
+        this.setState( { product: response.data } );
+    
+    }
+    
     render() {
-        return <h1>Product</h1>
+
+    //  Pegando a variável do estado
+        const { product } = this.state;
+
+        return (
+            <div className="product-info">
+                <h1>{ product.title }</h1>
+                <p>{ product.description }</p>
+
+                <p>
+                    URL: <a target="_blank" href={ product.url }>{ product.url }</a>
+                </p>
+            </div>
+        );
     }
 }
