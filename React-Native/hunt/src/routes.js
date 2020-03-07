4d2dbed80fe519ac4f852e3cc8366fe7bea5e2ef
Matheus Sousa Santos <matheus.ads.study@gmail.com>
2020-03-07 11:28:57 -0300

    ( React Native ) # Aula 05 - Estilizando Header e StatusBar

--- React-Native/hunt/src/routes.js
+++ React-Native/hunt/src/routes.js
@@ -4,4 +4,15 @@ import Main from './pages/main';
 
 export default createStackNavigator({
     Main,
+
+}, {
+    navigationOptions: {
+
+        headerStyle: {
+            backgroundColor: "#DA552F",
+        },
+
+        headerTintColor: "#FFF",
+
+    },
 }) 
