8f28569e5f68a630759f38faa7908d0bc3ac0f1d
Matheus Sousa Santos <matheus.ads.study@gmail.com>
2020-03-07 14:21:39 -0300

    ( React Native ) # Aula 09 - Estilizando lista de produtos

--- React-Native/hunt/src/pages/main.js
+++ React-Native/hunt/src/pages/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import api from '../services/api';
 
-import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class Main extends Component {
 
@@ -25,13 +25,13 @@ export default class Main extends Component {
     };
 
     renderItem = ({ item }) => (
-       <View>
+       <View style={ styles.productContainer }>
+
+           <Text style={ styles.productTitle } >{item.title}</Text>
+           <Text style={ styles.productDescription } >{item.description}</Text>
            
-           <Text>{item.title}</Text>
-           <Text>{item.description}</Text>
-           
-           <TouchableOpacity onPress={ () => {} }>
-                <Text>Acessar</Text>
+           <TouchableOpacity style={ styles.productButton }onPress={ () => {} }>
+                <Text style={ styles.productButtonText } >Acessar</Text>
             </TouchableOpacity>
 
        </View> 
@@ -40,8 +40,9 @@ export default class Main extends Component {
     render() {
         return(
 
-            <View>
+            <View style={ styles.container }>
                 <FlatList
+                    contentContainerStyle={ styles.list }
                     data={ this.state.docs }
                     keyExtractor={ item => item._id }
                     renderItem={ this.renderItem }
@@ -53,3 +54,55 @@ export default class Main extends Component {
     }
 
 }
+
+const styles = StyleSheet.create({
+   constainer: {
+        flex: 1,
+       backgroundColor: '#fafafa',
+   },
+
+   list: {
+        padding: 20,
+   },
+
+   productContainer: {
+       backgroundColor: '#FFF',
+       borderWidth: 1,
+       borderColor: '#DDD',
+       borderRadius: 5,
+       padding: 20,
+       marginBottom: 20
+   },
+
+   productTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333'
+   },
+
+   productDescription: {
+
+        fontSize: 16,
+        color: '#999',
+        marginTop: 5,
+        lineHeight: 24
+   },
+
+   productButton: {
+       height: 42,
+       borderRadius: 5,
+       borderWidth: 2,
+       borderColor: '#DA552F',
+       backgroundColor: 'transparent',
+       justifyContent: 'center',
+       alignItems:'center',
+       marginTop: 10,
+   },
+
+   productButtonText: {
+       fontSize: 16,
+       color: '#DA552F',
+       fontWeight: 'bold'
+   }
+
+});
